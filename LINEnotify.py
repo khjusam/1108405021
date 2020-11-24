dfc8c45c069a9799223a0811ff5e171779ab098d
khjusam <74238269+khjusam@users.noreply.github.com>
2020-11-24 16:51:19 +0800

    Add files via upload

--- LINEnotify.py
+++ LINEnotify.py
@@ -1,6 +1,6 @@
 import requests
 
-  def lineNotifyMessage(token, msg):
+def lineNotifyMessage(token, msg):
       headers = {
           "Authorization": "Bearer " + token, 
           "Content-Type" : "application/x-www-form-urlencoded"
@@ -10,9 +10,8 @@ import requests
       r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
       return r.status_code
 	
-  # 修改為你要傳送的訊息內容
-  message = 'Notify from LINE, HELLO WORLD'
+message = 'Notify from LINE, HELLO WORLD1111'
   # 修改為你的權杖內容
-  token = 'b3wPJEnF56Y2QPhhrXPOupFuRrgypwRkwxtq9x5OQcN'
+token = 'b3wPJEnF56Y2QPhhrXPOupFuRrgypwRkwxtq9x5OQcN'
 
-  lineNotifyMessage(token, message)
+lineNotifyMessage(token, message)
